6d6815605aeb4221224b35f3e40a78ed8463d17a
mirmashel <maximkras505@gmail.com>
2020-10-31 15:39:38 +0300

    revert to basel

--- CycleGAN/models/cycle_gan_model.py
+++ CycleGAN/models/cycle_gan_model.py
@@ -41,8 +41,7 @@ class CycleGANModel(BaseModel):
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
-            parser.add_argument('--use_SP', action = 'store_true', help = 'use similarity preserving loss')
-            parser.add_argument('--lambda_similarity', type=float, default=0, help='similarity preserving loss')
+
         return parser
 
     def __init__(self, opt):
@@ -67,7 +66,6 @@ class CycleGANModel(BaseModel):
             self.model_names = ['G_A', 'G_B', 'D_A', 'D_B']
         else:  # during test time, only load Gs
             self.model_names = ['G_A', 'G_B']
-
 
         # define networks (both Generators and discriminators)
         # The naming is different from those used in the paper.
