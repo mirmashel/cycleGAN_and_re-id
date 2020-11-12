6af1f5a50b9f51b806dc7c39e82f8970a4188948
mirmashel <maximkras505@gmail.com>
2020-11-12 10:52:31 +0300

    jup

--- stylegan/pipe.sh
+++ stylegan/pipe.sh
@@ -1,3 +1,4 @@
-python train.py --name first_attempt --source_path ../../datasets/JTA_parsed/ --target_path ../../datasets/DukeMTMC/ \
-	--lambda_cls 1.5 --lambda_idt 1.5 --lambda_prcp 2.5 \
+python train.py --name idt_15_prcp_20 --source_path ../../datasets/JTA_parsed/ --target_path ../../datasets/DukeMTMC/ \
+	--lambda_cls 1.5 --lambda_idt 15 --lambda_prcp 20 \
 	--sched --phase 10000 --sample_iters 500 --save_iters 10000 --path_vgg_weights vgg_weights --prefix_vgg_weights VGG_13 #--ckpt_name ./checkpoints/first_attempt/000010_8.model
+# big_idt_prcp - 10, 10
